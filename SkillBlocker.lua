bc561b5b91b72addcc6193fbff504a7f247823e6
adjutantt <36602558+adjutantt@users.noreply.github.com>
2020-04-14 00:28:07 +0300

    Fix: savedVars+LAM, lock textures; Feature: chat log, prehook flip-flop

--- SkillBlocker.lua
+++ SkillBlocker.lua
@@ -2,27 +2,46 @@ blocker = {}
 blocker.name = "SkillBlocker"
 blocker.lock = {}
 
+blocker.savedVariables = {}
+blocker.variableVersion = 3
+
+blocker.default = {
+	displayAlert = true,
+	logToChat = true
+}
+
 local LAM = LibAddonMenu2
+
+local flag = true -- flip-flop for prehook control
 
 local panelData = {
     type = "panel",
 	name = "Skill Blocker",
 	author = "@adjutant",
-	version = "1.0"
+	version = "1.0",
 	registerForDefaults = true
 }
 
 local optionsData = {
     [1] = {
         type = "checkbox",
-        name = "My Checkbox",
-       	tooltip = "Checkbox's tooltip text.",
-        getFunc = function() return true end,
-        setFunc = function(value) d(value) end,
+        name = "Display alert:",
+       	tooltip = "Display an alert on locked skill cast attempt",
+       	default = blocker.default.displayAlert,
+        getFunc = function() return blocker.savedVariables.displayAlert end,
+        setFunc = function(value) blocker.savedVariables.displayAlert = value end,
+    },
+
+    [2] = {
+    	type = "checkbox",
+    	name = "Log to chat:",
+    	tooltip = "Log to chat on locked skill cast attempt",
+    	default = blocker.default.logToChat,
+        getFunc = function() return blocker.savedVariables.logToChat end,
+        setFunc = function(value) blocker.savedVariables.logToChat = value end,
     },
 }
 
-local logToChat = true
 local currentHotbar
 
 local function drawLocks()
@@ -31,19 +50,23 @@ local function drawLocks()
             blocker.lock[i]:SetHidden(false)
             if blocker.lock[i].mainBarLocked then
                 blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/locked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_up.dds")
+                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_down.dds")
+                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/locked_over.dds")
             else
                 blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/unlocked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_up.dds")   
+                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_down.dds")
+                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/unlocked_over.dds")   
             end
         elseif currentHotbar == 1 then
             blocker.lock[i]:SetHidden(false)
             if blocker.lock[i].offBarLocked then
                 blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/locked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_up.dds")
+                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_down.dds")
+                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/locked_over.dds")
             else
                 blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/unlocked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_up.dds")    
+                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_down.dds") 
+                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/unlocked_over.dds")   
             end
         else
             blocker.lock[i]:SetHidden(true)
@@ -92,10 +115,20 @@ local function loadLocks()
   	blocker.lock[6]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton6, TOP, 0, -7)
 end
 
+local function alert()
+	if (blocker.savedVariables.displayAlert) then
+		ZO_Alert(UI_displayAlert_CATEGORY_ERROR, SOUNDS.CHAMPION_PENDING_POINTS_CLEARED, SI_TRADEACTIONRESULT62)
+	end
+	if blocker.savedVariables.logToChat then
+		d("[Skill Blocker]: \""..GetAbilityName(GetSlotBoundId(slotNum)).."\" is locked")
+	end
+end
+
 local function Initialize()
 	EVENT_MANAGER:UnregisterForEvent(blocker.name, EVENT_ADD_ON_LOADED)
-	LAM2:RegisterAddonPanel("SkillBlocker", panelData)
-	LAM2:RegisterOptionControls("SkillBlocker", optionsData)
+	blocker.savedVariables = ZO_SavedVars:NewAccountWide("SkillBlockerVars", blocker.variableVersion, nil, blocker.default)
+	LAM:RegisterAddonPanel("Skill Blocker", panelData)
+	LAM:RegisterOptionControls("Skill Blocker", optionsData)
   	currentHotbar = ACTION_BAR_ASSIGNMENT_MANAGER:GetCurrentHotbarCategory()
 
   	loadLocks()
@@ -110,20 +143,18 @@ local function Initialize()
     end)
 
   	ZO_PreHook("ZO_ActionBar_CanUseActionSlots", function()
-		slotNum = tonumber(debug.traceback():match('keybind = "ACTION_BUTTON_(%d)'))
-		if (currentHotbar == 0 and blocker.lock[slotNum - 2].mainBarLocked) or 
-	   	   (currentHotbar == 1 and blocker.lock[slotNum - 2].offBarLocked) then
-				ZO_Alert(UI_ALERT_CATEGORY_ERROR, SOUNDS.CHAMPION_PENDING_POINTS_CLEARED, SI_TRADEACTIONRESULT62)
-		--	if (logToChat) then
-		--		d("\""..GetAbilityName(GetSlotBoundId(slotNum)).."\" cast was prevented by Skill Blocker")
-		--	end
-				ZO_ActionBar_OnActionButtonUp(slotNum)
-			
-				return true
+  		flag = not flag -- Since ZO_ActionBar_CanUseActionSlots is called twice for each ability cast
+		if flag then
+			slotNum = tonumber(debug.traceback():match('keybind = "ACTION_BUTTON_(%d)'))
+			if (currentHotbar == 0 and blocker.lock[slotNum - 2].mainBarLocked) or 
+	   	    	(currentHotbar == 1 and blocker.lock[slotNum - 2].offBarLocked) then
+					ZO_ActionBar_OnActionButtonUp(slotNum)
+					alert()
+					return true
+			end
 		end
 	end)
 end
-
 
 local function OnAddOnLoaded(event, addonName)
 	if addonName == blocker.name then
