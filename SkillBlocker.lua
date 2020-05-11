1f85d4fd5a2e451c46ed3e674a13cb2dda07fcca
adjutantt <36602558+adjutantt@users.noreply.github.com>
2020-05-11 23:39:52 +0300

    add savedVariables :metal:

--- SkillBlocker.lua
+++ SkillBlocker.lua
@@ -7,12 +7,48 @@ local flag = true -- flip-flop for prehook control
 
 --==============================SavedVariables==============================--
 
-blocker.variableVersion = 3
+local variableVersion = 2
+local savedVarsName = "SkillBlockerVars"
 
-blocker.default = {
+local defaults = {
+	settingsAccountWide = false,
 	displayAlert = false,
-	logToChat = true
+	logToChat = true,
+	rememberLocks = true,
+
+	-- skills
+
+
+mainBarSaved = { 
+	[1] = false,
+ 	[2] = true, 
+	[3] = false, 
+	[4] = true, 
+	[5] = false, 
+	[6] = true
+},
+
+offBarSaved = { 
+	[1] = true,
+ 	[2] = false, 
+	[3] = true, 
+	[4] = false, 
+	[5] = true, 
+	[6] = false
 }
+
+
+}
+
+local function GetSettings()
+	if blocker.settings.settingsAccountWide then
+		return blocker.globalSettings
+    else
+		return blocker.settings
+  end
+end
+
+blocker.GetSettings = GetSettings
 
 --==============================LibAddonMenu==============================--
 
@@ -23,29 +59,57 @@ local panelData = {
 	name = "Skill Blocker",
 	author = "@adjutant",
 	version = "1.0",
-	registerForDefaults = true
+	-- website = "https://www.esoui.com/downloads/***",
+    -- feedback = "https://www.esoui.com/downloads/***#comments",
 }
 
 local optionsData = {
-    [1] = {
+
+	[1] = {
+		type = "checkbox",
+        name = "Remember locked skills?",
+        getFunc = function() return blocker.settings.rememberLocks end,
+        setFunc = function(value) blocker.settings.rememberLocks = value end,
+        width = "full",
+  	},
+
+	[2] = {
+		type = "checkbox",
+        name = "Enable account-wide settings:",
+        getFunc = function() return blocker.settings.settingsAccountWide end,
+        setFunc = function(value) blocker.settings.settingsAccountWide = value end,
+        width = "full",
+  	},
+
+  	[3] = {
+        type = "description",
+        title = nil,
+        text = "Note: this will make settings below account-wide, however locked skills remain per-character.",
+        width = "full",
+    },
+
+    [4] = { -- header: Use global variables?
+			type    = "header",
+			name    = nil,
+    },
+
+    [5] = {
         type = "checkbox",
         name = "Display alert:",
        	tooltip = "Display ZOS-like alert on locked skill cast attempt",
-       	default = blocker.default.displayAlert,
-        getFunc = function() return blocker.savedVariables.displayAlert end,
-        setFunc = function(value) blocker.savedVariables.displayAlert = value end,
+        getFunc = function() return blocker.GetSettings().displayAlert end,
+        setFunc = function(value) blocker.GetSettings().displayAlert = value end,
     },
 
-    [2] = {
+    [6] = {
     	type = "checkbox",
     	name = "Log to chat:",
     	tooltip = "Log to chat on locked skill cast attempt",
-    	default = blocker.default.logToChat,
-        getFunc = function() return blocker.savedVariables.logToChat end,
-        setFunc = function(value) blocker.savedVariables.logToChat = value end,
+        getFunc = function() return blocker.GetSettings().logToChat end,
+        setFunc = function(value) blocker.GetSettings().logToChat = value end,
     },
 
-    [3] = {
+    [7] = {
         type = "description",
         title = nil,
         text = "This addon is in active development. Feature requests and bug reports are very welcome! Please leave your comments at ESOUI.",
@@ -53,7 +117,14 @@ local optionsData = {
     },
 }
 
---==============================Functions==============================--
+--==============================Main==============================--
+
+local function loadSaved()
+	for i = 1, 6 do
+		blocker.lock[i].mainBarLocked = blocker.settings.mainBarSaved[i]
+		blocker.lock[i].offBarLocked = blocker.settings.offBarSaved[i]
+	end
+end
 
 local function drawLocks() -- refresh lock textures based on their state and current hotbar
     for i = 1, 6 do
@@ -89,17 +160,21 @@ local function toggleLock(lock) -- toggle lock state based on current hotbar
 	if currentHotbar == 0 then
     	if lock.mainBarLocked then
       		lock.mainBarLocked = false
+      		blocker.settings.mainBarSaved[lock.index] = false
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_UNLOCKED)
     	else
       		lock.mainBarLocked = true
+      		blocker.settings.mainBarSaved[lock.index] = true
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_LOCKED)
     	end
   	elseif currentHotbar == 1 then
     	if lock.offBarLocked then
       		lock.offBarLocked = false
+      		blocker.settings.offBarSaved[lock.index] = false
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_UNLOCKED)
     	else
       		lock.offBarLocked = true
+      		blocker.settings.offBarSaved[lock.index] = true
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_LOCKED)
     	end
   	end
@@ -127,10 +202,10 @@ local function loadLocks() -- register lock controls and anchors
 end
 
 local function alert() -- called on locked skill cast attempt. More to come...?
-	if (blocker.savedVariables.displayAlert) then
+	if (blocker.GetSettings().displayAlert) then
 		ZO_Alert(UI_displayAlert_CATEGORY_ERROR, SOUNDS.CHAMPION_PENDING_POINTS_CLEARED, SI_TRADEACTIONRESULT62)
 	end
-	if blocker.savedVariables.logToChat then
+	if blocker.GetSettings().logToChat then
 		d("[Skill Blocker]: \""..GetAbilityName(GetSlotBoundId(slotNum)).."\" is locked")
 	end
 end
@@ -150,13 +225,20 @@ local function Initialize()
 
 	ZO_CreateStringId("SI_BINDING_NAME_UNLOCK_ALL", "Unlock all skills")
 
-	blocker.savedVariables = ZO_SavedVars:NewAccountWide("SkillBlockerVars", blocker.variableVersion, nil, blocker.default)
+	blocker.settings        = ZO_SavedVars:New(             savedVarsName, variableVersion, "settings", defaults)
+  	blocker.globalSettings  = ZO_SavedVars:NewAccountWide(  savedVarsName,  variableVersion, "globals",  defaults)
+
+--	blocker.savedVariables = ZO_SavedVars:NewAccountWide("SkillBlockerVars", blocker.variableVersion, nil, blocker.default)
 	LAM:RegisterAddonPanel("Skill Blocker", panelData)
 	LAM:RegisterOptionControls("Skill Blocker", optionsData)
 
   	currentHotbar = ACTION_BAR_ASSIGNMENT_MANAGER:GetCurrentHotbarCategory() -- get initial bar on login
 
   	loadLocks()
+  	if (blocker.globalSettings.settingsAccountWide == false and blocker.globalSettings.rememberLocks) then
+		d("[Skill Blocker]: Some of your skills are locked!")
+  		loadSaved()
+  	end
   	drawLocks()
 
   	ACTION_BAR_ASSIGNMENT_MANAGER:RegisterCallback("CurrentHotbarUpdated", -- to keep currentHotbar relevant
@@ -172,12 +254,10 @@ local function Initialize()
 		if flag then
 			slotNum = tonumber(debug.traceback():match('keybind = "ACTION_BUTTON_(%d)')) -- get pressed button
 			if (slotNum == 9 or GetSlotBoundId(slotNum) == 0) then return false end -- break if consumable or empty slot
-		--	d(slotNum)
-		--	d(GetSlotBoundId(slotNum))
 			if (currentHotbar == 0 and blocker.lock[slotNum - 2].mainBarLocked) or 
 	   	    	(currentHotbar == 1 and blocker.lock[slotNum - 2].offBarLocked) then
-					alert()
-					return true
+					alert() -- notify player
+					return true -- ESO won't run ability press if PreHook returns true
 			end
 		end
 	end)
