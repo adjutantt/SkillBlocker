697db50117022080b47f7f00ea25348eb61533d2
adjutantt <36602558+adjutantt@users.noreply.github.com>
2020-05-13 14:55:34 +0300

    add Baertram's fixes

--- SkillBlocker.lua
+++ SkillBlocker.lua
@@ -1,16 +1,17 @@
-blocker = {}
+SkillBlocker = {}
 
-blocker.lock = {}
+SkillBlocker.lock = {}
 
 local ADDON_NAME = "SkillBlocker"
-local ADDON_VERSION	= "1.0a"
+local ADDON_VERSION	= "1.0b"
 local ESOUI_URL = "https://www.esoui.com/downloads/info2619-SkillBlocker.html"
 local currentHotbar
 local flag = true -- flip-flop for prehook control
+local slotNum
 
 --==============================SavedVariables==============================--
 
-local variableVersion = 4
+local variableVersion = 5
 local savedVarsName = "SkillBlockerVars"
 
 local defaults = {
@@ -40,14 +41,14 @@ local defaults = {
 }
 
 local function getSettings()
-	if blocker.settings.settingsAccountWide then
-		return blocker.globalSettings
+	if SkillBlocker.settings.settingsAccountWide then
+		return SkillBlocker.globalSettings
     else
-		return blocker.settings
+		return SkillBlocker.settings
   end
 end
 
-blocker.getSettings = getSettings
+SkillBlocker.getSettings = getSettings
 
 --==============================LibAddonMenu==============================--
 
@@ -66,16 +67,16 @@ local optionsData = {
 	[1] = {
 		type = "checkbox",
         name = "Remember locked skills?",
-        getFunc = function() return blocker.settings.rememberLocks end,
-        setFunc = function(value) blocker.settings.rememberLocks = value end,
+        getFunc = function() return SkillBlocker.settings.rememberLocks end,
+        setFunc = function(value) SkillBlocker.settings.rememberLocks = value end,
         width = "full",
   	},
 
 	[2] = {
 		type = "checkbox",
         name = "Enable account-wide settings:",
-        getFunc = function() return blocker.settings.settingsAccountWide end,
-        setFunc = function(value) blocker.settings.settingsAccountWide = value end,
+        getFunc = function() return SkillBlocker.settings.settingsAccountWide end,
+        setFunc = function(value) SkillBlocker.settings.settingsAccountWide = value end,
         width = "full",
         requiresReload = true
   	},
@@ -96,24 +97,24 @@ local optionsData = {
         type = "checkbox",
         name = "Display alert:",
        	tooltip = "Display ZOS-like alert on locked skill cast attempt",
-        getFunc = function() return blocker.getSettings().displayAlert end,
-        setFunc = function(value) blocker.getSettings().displayAlert = value end,
+        getFunc = function() return SkillBlocker.getSettings().displayAlert end,
+        setFunc = function(value) SkillBlocker.getSettings().displayAlert = value end,
     },
 
     [6] = {
     	type = "checkbox",
     	name = "Log to chat:",
     	tooltip = "Log to chat on locked skill cast attempt",
-        getFunc = function() return blocker.getSettings().logToChat end,
-        setFunc = function(value) blocker.getSettings().logToChat = value end,
+        getFunc = function() return SkillBlocker.getSettings().logToChat end,
+        setFunc = function(value) SkillBlocker.getSettings().logToChat = value end,
     },
 
     [7] = {
     	type = "checkbox",
     	name = "Alert on login:",
     	tooltip = "Display a chat message on login if you have saved locked skills",
-        getFunc = function() return blocker.getSettings().alertLogin end,
-        setFunc = function(value) blocker.getSettings().alertLogin = value end,
+        getFunc = function() return SkillBlocker.getSettings().alertLogin end,
+        setFunc = function(value) SkillBlocker.getSettings().alertLogin = value end,
     },
 
     [8] = {
@@ -128,37 +129,37 @@ local optionsData = {
 
 local function loadSaved()
 	for i = 1, 6 do
-		blocker.lock[i].mainBarLocked = blocker.settings.mainBarSaved[i]
-		blocker.lock[i].offBarLocked = blocker.settings.offBarSaved[i]
+		SkillBlocker.lock[i].mainBarLocked = SkillBlocker.settings.mainBarSaved[i]
+		SkillBlocker.lock[i].offBarLocked = SkillBlocker.settings.offBarSaved[i]
 	end
 end
 
 local function drawLocks() -- refresh lock textures based on their state and current hotbar
     for i = 1, 6 do
         if currentHotbar == 0 then -- main bar
-            blocker.lock[i]:SetHidden(false)
-            if blocker.lock[i].mainBarLocked then
-                blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/locked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_down.dds")
-                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/locked_over.dds")
+            SkillBlocker.lock[i]:SetHidden(false)
+            if SkillBlocker.lock[i].mainBarLocked then
+                SkillBlocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/locked_up.dds")
+                SkillBlocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_down.dds")
+                SkillBlocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/locked_over.dds")
             else
-                blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/unlocked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_down.dds")
-                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/unlocked_over.dds")   
+                SkillBlocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/unlocked_up.dds")
+                SkillBlocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_down.dds")
+                SkillBlocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/unlocked_over.dds")   
             end
         elseif currentHotbar == 1 then -- offbar
-            blocker.lock[i]:SetHidden(false)
-            if blocker.lock[i].offBarLocked then
-                blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/locked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_down.dds")
-                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/locked_over.dds")
+            SkillBlocker.lock[i]:SetHidden(false)
+            if SkillBlocker.lock[i].offBarLocked then
+                SkillBlocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/locked_up.dds")
+                SkillBlocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/locked_down.dds")
+                SkillBlocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/locked_over.dds")
             else
-                blocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/unlocked_up.dds")
-                blocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_down.dds") 
-                blocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/unlocked_over.dds")   
+                SkillBlocker.lock[i]:SetNormalTexture("/esoui/art/miscellaneous/unlocked_up.dds")
+                SkillBlocker.lock[i]:SetPressedTexture("/esoui/art/miscellaneous/unlocked_down.dds") 
+                SkillBlocker.lock[i]:SetMouseOverTexture("/esoui/art/miscellaneous/unlocked_over.dds")   
             end
         else
-            blocker.lock[i]:SetHidden(true) -- Werewolf, Volendrung, etc...
+            SkillBlocker.lock[i]:SetHidden(true) -- Werewolf, Volendrung, etc...
         end
     end
 end
@@ -167,21 +168,21 @@ local function toggleLock(lock) -- toggle lock state based on current hotbar
 	if currentHotbar == 0 then
     	if lock.mainBarLocked then
       		lock.mainBarLocked = false
-      		blocker.settings.mainBarSaved[lock.index] = false
+      		SkillBlocker.settings.mainBarSaved[lock.index] = false
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_UNLOCKED)
     	else
       		lock.mainBarLocked = true
-      		blocker.settings.mainBarSaved[lock.index] = true
+      		SkillBlocker.settings.mainBarSaved[lock.index] = true
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_LOCKED)
     	end
   	elseif currentHotbar == 1 then
     	if lock.offBarLocked then
       		lock.offBarLocked = false
-      		blocker.settings.offBarSaved[lock.index] = false
+      		SkillBlocker.settings.offBarSaved[lock.index] = false
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_UNLOCKED)
     	else
       		lock.offBarLocked = true
-      		blocker.settings.offBarSaved[lock.index] = true
+      		SkillBlocker.settings.offBarSaved[lock.index] = true
 	  		PlaySound(SOUNDS.INVENTORY_ITEM_LOCKED)
     	end
   	end
@@ -198,29 +199,29 @@ local function loadLocks() -- register lock controls and anchors
       	lock:SetNormalTexture("/esoui/art/miscellaneous/unlocked_up.dds")
       	lock:SetPressedTexture("/esoui/art/miscellaneous/locked_up.dds")
       	lock:SetHandler("OnClicked", toggleLock)
-      	blocker.lock[i] = lock
+      	SkillBlocker.lock[i] = lock
 	end
-	blocker.lock[1]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton1, TOP, 0, -7)
-  	blocker.lock[2]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton2, TOP, 0, -7)
-  	blocker.lock[3]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton3, TOP, 0, -7)
-  	blocker.lock[4]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton4, TOP, 0, -7)
-  	blocker.lock[5]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton5, TOP, 0, -7)
-  	blocker.lock[6]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton6, TOP, 0, -7)
+	SkillBlocker.lock[1]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton1, TOP, 0, -7)
+  	SkillBlocker.lock[2]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton2, TOP, 0, -7)
+  	SkillBlocker.lock[3]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton3, TOP, 0, -7)
+  	SkillBlocker.lock[4]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton4, TOP, 0, -7)
+  	SkillBlocker.lock[5]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton5, TOP, 0, -7)
+  	SkillBlocker.lock[6]:SetAnchor(BOTTOM, ZO_SkillsAssignableActionBarButton6, TOP, 0, -7)
 end
 
 local function alert() -- called on locked skill cast attempt. More to come...?
-	if (blocker.getSettings().displayAlert) then
+	if (SkillBlocker.getSettings().displayAlert) then
 		ZO_Alert(UI_displayAlert_CATEGORY_ERROR, SOUNDS.CHAMPION_PENDING_POINTS_CLEARED, SI_TRADEACTIONRESULT62)
 	end
-	if blocker.getSettings().logToChat then
+	if SkillBlocker.getSettings().logToChat then
 		d("[Skill Blocker]: \""..GetAbilityName(GetSlotBoundId(slotNum)).."\" is locked")
 	end
 end
 
-function blocker.unlockAll() -- hotkey
+function SkillBlocker.unlockAll() -- hotkey
 	for i = 1, 6 do
-		blocker.lock[i].mainBarLocked = false
-		blocker.lock[i].offBarLocked = false
+		SkillBlocker.lock[i].mainBarLocked = false
+		SkillBlocker.lock[i].offBarLocked = false
 	end
 	drawLocks()
 	d("[Skill Blocker]: all skills unlocked")
@@ -232,8 +233,8 @@ local function Initialize()
 
 	ZO_CreateStringId("SI_BINDING_NAME_UNLOCK_ALL", "Unlock all skills")
 
-	blocker.settings = ZO_SavedVars:New(savedVarsName, variableVersion, "settings", defaults)
-  	blocker.globalSettings = ZO_SavedVars:NewAccountWide(savedVarsName, variableVersion, "globals", defaults)
+	SkillBlocker.settings = ZO_SavedVars:NewCharacterIdSettings(savedVarsName, variableVersion, "settings", defaults, GetWorldName())
+  	SkillBlocker.globalSettings = ZO_SavedVars:NewAccountWide(savedVarsName, variableVersion, "globals", defaults, GetWorldName(), "AllAccountsTheSame")
 
 	LAM:RegisterAddonPanel("Skill Blocker", panelData)
 	LAM:RegisterOptionControls("Skill Blocker", optionsData)
@@ -242,11 +243,11 @@ local function Initialize()
 
   	loadLocks()
 
-  	if (blocker.getSettings().rememberLocks) then
+  	if (SkillBlocker.getSettings().rememberLocks) then
   		loadSaved()
-  		if (blocker.getSettings().alertLogin) then
+  		if (SkillBlocker.getSettings().alertLogin) then
   			for i = 1, 6 do
-  				if (blocker.lock[i].mainBarLocked or blocker.lock[i].offBarLocked) then
+  				if (SkillBlocker.lock[i].mainBarLocked or SkillBlocker.lock[i].offBarLocked) then
   					d("[Skill Blocker]: Some of your skills are locked!")
   					break
   				end
@@ -269,8 +270,8 @@ local function Initialize()
 		if flag then
 			slotNum = tonumber(debug.traceback():match('keybind = "ACTION_BUTTON_(%d)')) -- get pressed button
 			if (slotNum == 9 or GetSlotBoundId(slotNum) == 0) then return false end -- break if consumable or empty slot
-				if (currentHotbar == 0 and blocker.lock[slotNum - 2].mainBarLocked) or 
-	   	    	(currentHotbar == 1 and blocker.lock[slotNum - 2].offBarLocked) then
+				if (currentHotbar == 0 and SkillBlocker.lock[slotNum - 2].mainBarLocked) or 
+	   	    	(currentHotbar == 1 and SkillBlocker.lock[slotNum - 2].offBarLocked) then
 					alert() -- notify player
 					return true -- ESO won't run ability press if PreHook returns true
 			end
